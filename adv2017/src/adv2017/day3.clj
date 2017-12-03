8a98613a44177286aae58dd379c321e463d6af40
Anders Engström <anders.engstrom@leogears.com>
2017-12-03 18:23:45 +0100

    Added solution for part 2, day 3

--- adv2017/src/adv2017/day3.clj
+++ adv2017/src/adv2017/day3.clj
@@ -21,19 +21,55 @@
   Return a new coord `[x y]`."
   [[x y :as coord] direction]
   (case direction
-    :right (map + coord [0 1])
-    :up    (map + coord [1 0])
-    :left  (map + coord [0 -1])
-    :down  (map + coord [-1 0])))
+    :right (map + coord [1 0])
+    :up    (map + coord [0 1])
+    :left  (map + coord [-1 0])
+    :down  (map + coord [0 -1])))
+
+(defn coordinates
+  "Get an infinite list of coordinates by moving in a counter-clockwise circle."
+  []
+  (reductions move [0 0] (steps-seq)))
 
 (defn square->dist
   "Calculate the distance to square 1 from `square`."
   [square]
-  (time
-   (let [coords (reductions move [0 0] (steps-seq))
-         [x y]  (first (drop (dec square) coords))]
-     (+ (Math/abs x) (Math/abs y)))))
+  (let [coords (coordinates)
+        [x y]  (first (drop (dec square) coords))]
+    (+ (Math/abs x) (Math/abs y))))
 
-(defn results []
-  (let [dist 277678]
-    (println "Distance for" dist ":" (square->dist dist))))
+(defn neighbours
+  "Get the neighbours (as seq of `[x y]` coordinates)
+  of `coord`."
+  [[x y :as coord]]
+  (let [diffs (for [x     [-1 0 1]
+                    y     [-1 0 1]
+                    :when (not= x y 0)] [x y])]
+    (map #(map + coord %) diffs)))
+
+(defn next-state
+  "Given a state `{[x y] v1 ...}` and an `[x y]` coordinate -
+  calculate the new state with `xy` included."
+  [state xy]
+  (let [adjecent (neighbours xy)
+        value (reduce + 0 (map (fn [xy] (get state xy 0)) adjecent))]
+    (assoc state xy value)))
+
+(defn grid-values
+  "Values in the crid (calculated by summing values from neighbours)"
+  []
+  (let [coords (coordinates)
+        incr-states (reductions next-state
+                                {[0 0] 1}
+                                (drop 1 (coordinates)))]
+    (map #(get %1 %2 0) incr-states coords)))
+
+(defn results
+  "Print the results."
+  []
+  (time
+   (let [input 277678
+         solution-1 (square->dist input)
+         solution-2 (first (drop-while #(< % input) (grid-values)))]
+     (println "Distance for" input ":" solution-1)
+     (println "Value larger than" solution-1 "in adjecent-values grid:" solution-2))))

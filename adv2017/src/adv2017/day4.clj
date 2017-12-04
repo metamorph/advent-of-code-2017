b28261510f2bdae9f04461c67c1bdf985d420078
Anders Engström <anders.engstrom@leogears.com>
2017-12-04 07:55:06 +0100

    Day 4 - Puzzle 2
    (and some minor refactorings)

--- adv2017/src/adv2017/day4.clj
+++ adv2017/src/adv2017/day4.clj
@@ -1,13 +1,23 @@
 (ns adv2017.day4
-  (:require [clojure.java.io :refer [reader resource]]
+  (:require [adv2017.day2 :as day2]
+            [clojure.java.io :refer [reader resource]]
             [clojure.string :refer [split]]))
 
-(defn simple-checksum? [words]
-  (= (count words)
-     (count (set words))))
+(defn anagrams? [w1 w2]
+  (= (frequencies w1) (frequencies w2)))
 
-(defn solution-1 []
+(defn simple-checksum? [words]
+  (= (count words) (count (set words))))
+
+(defn anagram-checksum? [words]
+  (empty? (filter #(apply anagrams? %) (day2/combinations words))))
+
+(defn count-valid [checksum-pred]
   (with-open [reader (reader (resource "day4-input.txt"))]
     (->> (line-seq reader)
-        (filter #(simple-checksum? (split % #"\s+")))
-        (count))))
+         (filter #(checksum-pred (split % #"\s+")))
+         (count))))
+
+(defn prn-solutions []
+  (println "Valid checksums, part 1:" (count-valid simple-checksum?))
+  (println "Valid checksums, part 2:" (count-valid anagram-checksum?)))

0e2a33e71e2de0afd826d6d28744f404184da4e9
Anders Engström <anders.engstrom@leogears.com>
2017-12-08 08:14:32 +0100

    WIP: Almost there

--- adv2017/src/adv2017/day8.clj
+++ adv2017/src/adv2017/day8.clj
@@ -6,11 +6,11 @@
    "c dec -10 if a >= 1"
    "c inc -20 if c == 10"])
 
-(defn parse-line
+(defn parse-instr
   "TODO: Maybe try antlr instead, just for fun."
   [l]
   (if-let [[_ reg op v pred-reg pred-op pred-v]
-           (re-find #"(\w+)\s+(\w+)\s+(\d+)\s+if\s+(\w+)\s+(\W+)\s+(\d+)" l)]
+           (re-find #"(\w+)\s+(\w+)\s+([-]*\d+)\s+if\s+(\w+)\s+(\W+)\s+(\d+)" l)]
     {:reg   reg
      :op    op
      :value (Integer/parseInt v)
@@ -21,11 +21,35 @@
 (defn make-pred
   "Creates a predicate that checks if the registry is in a specific state"
   [{:keys [reg op value]}]
-  (let [f (case op ">" >
-                "<" <
-                ">=" >=
-                "<=" <=
-                "==" =
-                "!=" not=)]
+  (let [f (case op
+            ">"  >
+            "<"  <
+            ">=" >=
+            "<=" <=
+            "==" =
+            "!=" not=)]
     (fn [registry]
       (f (get registry reg 0) value))))
+
+(defn make-op
+  "Creates an operation to apply to the state."
+  [{:keys [reg op value]}]
+  (let [f (case op
+            "inc" +
+            "dec" -)]
+    (fn [registry]
+      (let [v (get registry reg 0)]
+        (assoc registry reg (f v value))))))
+
+(defn reducer-fn [registry instr]
+  (let [pred-fn (make-pred (:pred instr))
+        op-fn   (make-op instr)]
+    (if (pred-fn registry)
+      (op-fn registry)
+      registry)))
+
+(defn apply-instr [instructions]
+  (reduce reducer-fn {} instructions))
+
+(defn solve-1 [lines]
+  (apply max (vals (apply-instr (map parse-instr lines)))))

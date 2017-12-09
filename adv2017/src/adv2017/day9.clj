1913d003d9c3217a08b97d2f114d2df5ac4011a6
Anders Engström <anders.engstrom@leogears.com>
2017-12-09 22:17:26 +0100

    Day 9 - complete

--- adv2017/src/adv2017/day9.clj
+++ adv2017/src/adv2017/day9.clj
@@ -1,4 +1,5 @@
-(ns adv2017.day9)
+(ns adv2017.day9
+  (:require [clojure.java.io :as io]))
 
 (defn make-drop-!-xf
   "Returns a transducer that will drop any `!` and a
@@ -29,7 +30,31 @@
          :else    (not @drop-next?))))))
 
 (defn group-score [chars]
-  (let [cleaned-up (into '() (comp (make-drop-!-xf)
-                                   (make-drop-garbage-xf)) chars)]
+  (time
+   (let [cleaned-up (eduction (comp (make-drop-!-xf)
+                                    (make-drop-garbage-xf)) chars)]
+     (last
+      (reduce (fn [[opened sum :as state] c]
+                (case c
+                  \{ [(inc opened) sum]
+                  \} [(dec opened) (+ sum opened)]
+                  state))
+              [0 0]
+              cleaned-up)))))
 
-    ))
+(defn count-garbage [chars]
+  (time
+   (let [cleaned-up (eduction (make-drop-!-xf) chars)]
+     (last
+      (reduce (fn [[count? sum :as state] c]
+                (cond
+                  (and (not count?) (= \< c)) [true sum]
+                  (= \> c)                    [false sum]
+                  count?                      [count? (inc sum)]
+                  :else                       state))
+              [false 0]
+              cleaned-up)))))
+
+(defn read-input []
+  (->> (io/resource "day9-input.txt")
+       (slurp)))

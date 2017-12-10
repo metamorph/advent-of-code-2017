d227f7c2c2d46d536e5c2a2f5c9d4e287163df4d
Anders Engström <anders.engstrom@leogears.com>
2017-12-10 23:42:55 +0100

    Day 10 - first level

--- adv2017/src/adv2017/day10.clj
+++ adv2017/src/adv2017/day10.clj
@@ -5,6 +5,9 @@
 (def test-input
   [3 4 1 5])
 
+(def day10-marks (take 256 (iterate inc 0)))
+(def day10-input [197, 97, 204, 108, 1, 29, 5, 71, 0, 50, 2, 255, 248, 78, 254, 63])
+
 (defn sub-list
   [v idx len]
   {:pre [(>= (count v) len)]}
@@ -20,3 +23,14 @@
       (insert-list result head? 0)
       result)))
 
+(defn map-step [marks idx len skip]
+  (let [sub (reverse (sub-list marks idx len))
+        result (insert-list marks sub idx)
+        next-idx (mod (+ idx len skip) (count marks))]
+    [result next-idx]))
+
+(defn apply-knot-hash [marks lens]
+  (reduce (fn [[marks idx] [len skip]]
+            (map-step marks idx len skip))
+          [marks 0]
+          (map vector lens (iterate inc 0))))

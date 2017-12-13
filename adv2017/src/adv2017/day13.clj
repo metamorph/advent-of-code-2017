af9d1bbc6b5a2451f4ecd5d78582f7a743d140e7
Anders Engström <anders.engstrom@leogears.com>
2017-12-13 08:24:50 +0100

    Day 13 - step 1

---
+++ adv2017/src/adv2017/day13.clj
@@ -0,0 +1,34 @@
+(ns adv2017.day13
+  (:require [clojure.java.io :as io]))
+
+(def test-fw [[0 3]
+              [1 4]
+              [4 6]
+              [6 4]])
+
+(defn line->vec [l]
+  (let [[_ l r] (re-find #"(\d+):\s+(\d+)" l)]
+    [(Integer/parseInt l) (Integer/parseInt r)]))
+
+(defn read-input []
+  (->> (io/reader (io/resource "day13-input.txt"))
+       (line-seq)
+       (map line->vec)))
+
+(defn cycle-length
+  "Get the length of a cycle for a given `range`.
+  The cycle-length is the number of picoseconds between
+  passing the same distance."
+  [range] (* 2 (dec range)))
+
+(defn at-pos?
+  "Check if a sensor with range (`r`) is at the position
+  `pos` at a given `time`."
+  [r pos time]
+  (zero? (rem (+ pos time) (cycle-length r))))
+
+(defn trip-severity [fw]
+  (let [hit-ranges (filter
+                    (fn [[l r]] (at-pos? r 0 l)) fw)]
+    (reduce (fn [acc [l r]] (+ acc (* l r))) 0 hit-ranges)))
+

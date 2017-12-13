208c0ced373406f500db8fe856b5f887aa77d914
Anders Engström <anders.engstrom@leogears.com>
2017-12-13 21:45:40 +0100

    Fixing second puzzle for day 10
    ... and what a mess.

--- adv2017/src/adv2017/day10.clj
+++ adv2017/src/adv2017/day10.clj
@@ -1,4 +1,5 @@
-(ns adv2017.day10)
+(ns adv2017.day10
+  (:require [clojure.string :as str]))
 
 (def test-list
   [0 1 2 3 4])
@@ -7,6 +8,10 @@
 
 (def day10-marks (take 256 (iterate inc 0)))
 (def day10-input [197, 97, 204, 108, 1, 29, 5, 71, 0, 50, 2, 255, 248, 78, 254, 63])
+
+(def puzzle-input "197,97,204,108,1,29,5,71,0,50,2,255,248,78,254,63")
+(def suffix '(17 31 73 47 23))
+(defn str->ascii [s] (map int s))
 
 (defn sub-list
   [v idx len]
@@ -29,8 +34,33 @@
         next-idx (mod (+ idx len skip) (count marks))]
     [result next-idx]))
 
-(defn apply-knot-hash [marks lens]
-  (reduce (fn [[marks idx] [len skip]]
-            (map-step marks idx len skip))
-          [marks 0]
-          (map vector lens (iterate inc 0))))
+(defn apply-knot-hash
+  ([marks lens] (apply-knot-hash marks lens 0 (iterate inc 0)))
+  ([marks lens start-index step-seq]
+   (reduce (fn [[marks idx] [len skip]]
+                    (map-step marks idx len skip))
+                  [marks start-index]
+                  (map vector lens step-seq))))
+
+(defn sparse-hash [marks lens iterations]
+  (loop [step-seq (iterate inc 0)
+         idx 0
+         marks marks
+         pass 0]
+    (if (= pass iterations)
+      marks
+      (let [[m i] (apply-knot-hash marks lens idx (take (count lens) step-seq))]
+        (recur (drop (count lens) step-seq) i m (inc pass))))))
+
+(defn dense-hash [sparse-hash]
+  (map #(apply bit-xor %) (partition 16 sparse-hash)))
+
+(defn byte->hex [b]
+  (format "%02x" b))
+
+
+(defn solve-2 [s]
+  (let [lens (concat (str->ascii s) suffix)
+        sparse (sparse-hash day10-marks lens 64)
+        dense  (dense-hash sparse)]
+    (str/join (map byte->hex dense))))

7587ef16596763b7e4c2320add19a4b54263b4f7
Anders Engström <anders.engstrom@leogears.com>
2017-12-17 20:21:23 +0100

    Day 17 - puzzle 2

--- adv2017/src/adv2017/day17.clj
+++ adv2017/src/adv2017/day17.clj
@@ -6,8 +6,8 @@
   (let [[head tail] (split-at idx xs)]
     (into [] (concat head (vector v) tail))))
 
-(defn step [xs idx len]
-  (mod (+ idx len) (count xs)))
+(defn step [size idx len]
+  (mod (+ idx len) size))
 
 (defn solve-1 [step-size iterations]
   (let [[xs idx]
@@ -17,7 +17,27 @@
                idx 0]
           (if (= it iterations)
             [xs idx]
-            (let [next-idx (inc (step xs idx step-size))]
+            (let [next-idx (inc (step (count xs) idx step-size))]
               (recur (inc it) (inc v) (insert-at xs next-idx v) next-idx))))]
+    (get xs (inc idx))))
 
-    (println "Value after 2017:" (get xs (inc idx)))))
+(defn solve-2
+  "Skip mutating a list, simply iterate over a growing length, and record
+  indexes where values will be inserted. Store the last value inserted at position `1`.
+  After all iterations this will be our value (since we're never inserting at position `0`.)"
+  [step-size iterations]
+  (loop [value-at-1 nil
+         it 0
+         v 1
+         len 1
+         idx 0]
+    (if (= it iterations)
+      value-at-1
+      (let [next-idx (inc (step len idx step-size))]
+        (recur (if (= 1 next-idx) v value-at-1)
+               (inc it)
+               (inc v)
+               (inc len)
+                next-idx)))))
+
+
